8a4c0aca580ca8b306e9b27bbc3e3deee9cb0571
Pentapati Sai Karthikey (sp53252) <sp53252@austin.utexas.edu@ece-a52117.austin.utexas.edu>
2023-10-05 22:56:32 -0500

    pretty good

--- simplification.py
+++ simplification.py
@@ -7,7 +7,7 @@ from util.mesh import Mesh
 def get_parser():
     parser = argparse.ArgumentParser(description="Mesh Simplification")
     parser.add_argument("-i", "--input", type=str, required=True)
-    parser.add_argument("--v", type=int, required=True)
+    parser.add_argument("--f", type=int, required=True)
     args = parser.parse_args()
     return args
 
@@ -15,15 +15,14 @@ def main():
     args = get_parser()
     mesh = Mesh(args.input)
     mesh_name = os.path.basename(args.input).split(".")[-2]
-    if args.v >= mesh.vs.shape[0]:
+    if args.f >= mesh.vs.shape[0]:
         print("[ERROR]: Target vertex number should be smaller than {}!".format(mesh.vs.shape[0]))
         exit()
     tx = time.time()
-    simp_mesh = mesh.simplification(target_v=args.v)
-    print(time.time()-tx)
-    # simp_mesh = mesh.edge_based_simplification(target_v=args.v, valence_aware=args.optim)
+    simp_mesh = mesh.simplification(target_f=args.f)
+    print("Time taken:",time.time()-tx)
     os.makedirs("data/output/", exist_ok=True)
-    simp_mesh.save("data/output/{}_{}.obj".format(mesh_name, simp_mesh.vs.shape[0]))
+    simp_mesh.save("data/output/{}_{}.obj".format(mesh_name, args.f))
     print("[FIN] Simplification Completed!")
 
 if __name__ == "__main__":
--- util/mesh.py
+++ util/mesh.py
@@ -12,29 +12,20 @@ OPTIM_VALENCE = 6
 VALENCE_WEIGHT = 1
 
 class Mesh:
-    def __init__(self, path, boundary_weight=100):
+    def __init__(self, path):
         self.path = path
         mesh = load_objs_as_meshes([path])
         textures = mesh.textures
 
         verts = mesh.verts_packed().numpy()
         faces = mesh.faces_packed().numpy()
-
         ft = textures.faces_uvs_padded().numpy()[0,:,:]
         vt = textures.verts_uvs_padded().numpy()[0,:,:]
         self.texture_map = textures.maps_padded()[0]
 
         self.clean_mesh(faces, verts, ft, vt)
-        ndim = self.vs.shape[1]
+        self.ndim = self.vs.shape[1]
 
-        self.A_s = [np.zeros((ndim,ndim)) for _ in range(len(self.vs))]
-        self.B_s = [np.zeros((ndim,1)) for _ in range(len(self.vs))]
-        self.C_s = [np.zeros((1,1)) for _ in range(len(self.vs))]
-
-        self.build_v2v()
-        self.build_vf()
-        self.get_abcd()
-        self.build_boundary_quadratics(boundary_weight)
         self.simp = False
 
     def clean_mesh(self, faces, verts, ft, vt):
@@ -74,7 +65,7 @@ class Mesh:
             v2v[e[1]].append(e[0])
         self.v2v = v2v
 
-    def get_abcd(self):
+    def get_abc(self):
         self.p = self.vs[self.faces[:, 0]]
         self.e1 = self.vs[self.faces[:, 1]] - self.p
         self.e1 /= la.norm(self.e1, axis=1, keepdims=True) + 1e-24
@@ -83,6 +74,27 @@ class Mesh:
         e2_p_e1 = (e2*self.e1).sum(axis=1, keepdims=True)*self.e1
         self.e2 = e2 - e2_p_e1
         self.e2 /= la.norm(self.e2, axis=1, keepdims=True) + 1e-24
+
+        self.A_s = [np.zeros((self.ndim,self.ndim)) for _ in range(len(self.vs))]
+        self.B_s = [np.zeros((self.ndim,1)) for _ in range(len(self.vs))]
+        self.C_s = [np.zeros((1,1)) for _ in range(len(self.vs))]
+
+        for i, v in enumerate(self.vs):
+            f_s = list(self.vf[i])
+            nf = len(self.vf[i])
+            for f in f_s:
+                e1, e2, p = self.e1[f][:,None], self.e2[f][:,None], self.p[f][:,None]
+                
+                pe1 = np.sum(p * e1)
+                pe2 = np.sum(p * e2)
+
+                self.A_s[i] += np.eye(self.ndim)-np.matmul(e1, e1.T)-np.matmul(e2,e2.T)
+                self.B_s[i] += e1*pe1 + e2*pe2 - p
+                self.C_s[i] += np.matmul(p.T, p) - pe1**2 - pe2**2
+            self.A_s[i] /= nf
+            self.B_s[i] /= nf
+            self.C_s[i] /= nf
+            # E = np.matmul(v.T,np.matmul(self.A_s[i], v)) + 2*np.matmul(self.B_s[i].T, v) + self.C_s[i]
 
     def build_boundary_quadratics(self, boundary_weight=100):
         ef = [[] for _ in range(len(self.edges))]
@@ -107,34 +119,17 @@ class Mesh:
                 self.C_s[v0] += C
                 self.C_s[v1] += C
                 
-    def simplification(self, target_v):
-        vs, vf, edges = self.vs, self.vf, self.edges
-        ndim = vs.shape[1]
+    def simplification(self, target_f, boundary_weight=100):
+        self.build_v2v()
+        self.build_vf()
+        self.get_abc()
+        self.build_boundary_quadratics(boundary_weight)
 
-        """ 1. compute Q for each vertex """
-        for i, v in enumerate(vs):
-            f_s = list(vf[i])
-            nf = len(vf[i])
-            for f in f_s:
-                e1, e2, p = self.e1[f][:,None], self.e2[f][:,None], self.p[f][:,None]
-                
-                pe1 = np.sum(p * e1)
-                pe2 = np.sum(p * e2)
-
-                self.A_s[i] += np.eye(ndim)-np.matmul(e1, e1.T)-np.matmul(e2,e2.T)
-                self.B_s[i] += e1*pe1 + e2*pe2 - p
-                self.C_s[i] += np.matmul(p.T, p) - pe1**2 - pe2**2
-            self.A_s[i] /= nf
-            self.B_s[i] /= nf
-            self.C_s[i] /= nf
-            # E = np.matmul(v.T,np.matmul(self.A_s[i], v)) + 2*np.matmul(self.B_s[i].T, v) + self.C_s[i]
-        """ 2. compute E for every possible pairs and create heapq """
+        """ Compute E for every possible pairs and create heapq """
         E_heap = []
-        for i, e in enumerate(edges):
-            v_0, v_1 = vs[e[0]], vs[e[1]]
-            A_new = (self.A_s[e[0]]+self.A_s[e[1]])
-            B_new = (self.B_s[e[0]]+self.B_s[e[1]])
-            C_new = (self.C_s[e[0]]+self.C_s[e[1]])
+        for i, e in enumerate(self.edges):
+            v_0, v_1 = self.vs[e[0]], self.vs[e[1]]
+            A_new,B_new,C_new = self.A_s[e[0]]+self.A_s[e[1]], self.B_s[e[0]]+self.B_s[e[1]], self.C_s[e[0]]+self.C_s[e[1]]
 
             try:
                 v_new = -np.matmul(la.inv(A_new), B_new)
@@ -142,75 +137,50 @@ class Mesh:
                 v_new = (0.5 * (v_0 + v_1))[:,None]
 
             E_new = np.matmul(v_new.T,np.matmul(A_new, v_new)) + 2*np.matmul(B_new.T, v_new) + C_new
-            # heapq.heappush(E_heap, (E_new[0,0], (e[0], e[1]), v_new[:,0], (A_new, B_new, C_new)))
             E_heap.append((E_new[0,0], (e[0], e[1]), v_new[:,0]))
         E_heap.sort()
 
-        """ 3. collapse minimum-error vertex """
+        """ Collapse minimum-error vertex """
         simp_mesh = copy.deepcopy(self)
-
         vi_mask = np.ones([len(simp_mesh.vs)]).astype(np.bool_)
         fi_mask = np.ones([len(simp_mesh.faces)]).astype(np.bool_)
-
         vert_map = [{i} for i in range(len(simp_mesh.vs))]
-
-        while np.sum(vi_mask) > target_v:
-            if len(E_heap) == 0:
-                print("[Warning]: edge cannot be collapsed anymore!")
-                break
-
-            # E_0, (vi_0, vi_1), v_new, Q_new = heapq.heappop(E_heap)
+        while np.sum(fi_mask) > target_f and len(E_heap)>0:
             E_0, (vi_0, vi_1), v_new = E_heap.pop(0)
-
             if (vi_mask[vi_0] == False) or (vi_mask[vi_1] == False):
                 continue
-
+            
             """ edge collapse """
-            shared_vv = list(set(simp_mesh.v2v[vi_0]).intersection(set(simp_mesh.v2v[vi_1])))
             merged_faces = simp_mesh.vf[vi_0].intersection(simp_mesh.vf[vi_1])
-
-            if len(shared_vv) != 2:
-                """ non-manifold! """
-                # print("non-manifold can be occured!!" , len(shared_vv))
-                continue
-
-            elif len(merged_faces) != 2:
-                """ boundary """
-                # print("boundary edge cannot be collapsed!")
-                continue
-
-            else:
-                simp_mesh.vs[vi_0] = v_new
-                # simp_mesh.A_s[vi_0], simp_mesh.B_s[vi_0], simp_mesh.C_s[vi_0] = Q_new
-                self.edge_collapse(simp_mesh, vi_0, vi_1, merged_faces, vi_mask, fi_mask, vert_map, E_heap)
-                # print(np.sum(vi_mask), np.sum(fi_mask))
+            simp_mesh.vs[vi_0] = v_new
+            vi_mask[vi_1] = False
+            fi_mask[np.array(list(merged_faces)).astype(np.int64)] = False
+            vert_map[vi_0] = vert_map[vi_0].union(vert_map[vi_1])
+            vert_map[vi_0] = vert_map[vi_0].union({vi_1})
+            vert_map[vi_1] = set()
+            self.edge_collapse(simp_mesh, (vi_0, vi_1), E_heap)
         
         self.rebuild_mesh(simp_mesh, vi_mask, fi_mask, vert_map)
-        simp_mesh.simp = True
-        
         return simp_mesh
 
-    def edge_collapse(self, simp_mesh, vi_0, vi_1, merged_faces, vi_mask, fi_mask, vert_map, E_heap):
+    @staticmethod
+    def edge_collapse(simp_mesh, edge, E_heap):
+        vi_0, vi_1 = edge
+        merged_faces = simp_mesh.vf[vi_0].intersection(simp_mesh.vf[vi_1])
         shared_vv = list(set(simp_mesh.v2v[vi_0]).intersection(set(simp_mesh.v2v[vi_1])))
         new_vi_0 = set(simp_mesh.v2v[vi_0]).union(set(simp_mesh.v2v[vi_1])).difference({vi_0, vi_1})
         simp_mesh.vf[vi_0] = simp_mesh.vf[vi_0].union(simp_mesh.vf[vi_1]).difference(merged_faces)
         simp_mesh.vf[vi_1] = set()
-        simp_mesh.vf[shared_vv[0]] = simp_mesh.vf[shared_vv[0]].difference(merged_faces)
-        simp_mesh.vf[shared_vv[1]] = simp_mesh.vf[shared_vv[1]].difference(merged_faces)
+        for vv in shared_vv:
+            simp_mesh.vf[vv] = simp_mesh.vf[vv].difference(merged_faces)
 
         simp_mesh.v2v[vi_0] = list(new_vi_0)
         for v in simp_mesh.v2v[vi_1]:
             if v != vi_0:
                 simp_mesh.v2v[v] = list(set(simp_mesh.v2v[v]).difference({vi_1}).union({vi_0}))
         simp_mesh.v2v[vi_1] = []
-        vi_mask[vi_1] = False
 
-        vert_map[vi_0] = vert_map[vi_0].union(vert_map[vi_1])
-        vert_map[vi_0] = vert_map[vi_0].union({vi_1})
-        vert_map[vi_1] = set()
         
-        fi_mask[np.array(list(merged_faces)).astype(np.int64)] = False
-
         invalid_heap_idxs = [i for i,E in enumerate(E_heap) if vi_0 in E[1] or vi_1 in E[1]]
         invalid_heap_idxs.sort(reverse=True)
         for idx in invalid_heap_idxs: E_heap.pop(idx)
@@ -227,7 +197,6 @@ class Mesh:
 
             E_new = np.matmul(v_mid.T,np.matmul(A_new, v_mid)) + 2*np.matmul(B_new.T, v_mid) + C_new
             insort(E_heap, (E_new[0,0], (vi_0, vv_i), v_mid[:,0]))
-            # heapq.heappush(E_heap, (E_new[0,0], (vi_0, vv_i), v_mid[:,0], (A_new, B_new, C_new)))
 
     @staticmethod
     def rebuild_mesh(simp_mesh, vi_mask, fi_mask, vert_map):
@@ -243,7 +212,6 @@ class Mesh:
         #     if len(vmap)>1:
         #         a = []
         #         for v in vmap: a.append(vert_dict[v])
-        #         print('vmap:',vmap,'simp vmap',a)
         #     v_corrected = np.zeros(3)
         #     c=0
         #     for v in vmap:
@@ -266,6 +234,8 @@ class Mesh:
         for i, f in enumerate(simp_mesh.faces):
             for j in range(3):
                 simp_mesh.faces[i][j] = face_map[f[j]]
+        
+        simp_mesh.simp = True
 
     def save(self,filename):
         vertices = torch.tensor(self.verts)
